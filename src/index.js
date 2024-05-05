3449a269177b87e118591564c6f8de1a9de60844
rahul p. <rahul.sanswebsolutions@gmail.com>
2024-05-05 15:29:00 +0530

    update code

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+// const app = express()
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* (async () => {
+     try {
+
+         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+         app.on("error", (error) => {
+             console.log("error", error);
+             throw error;
+         })
+         app.listen(process.env.PORT, () => {
+             console.log(`app is listening at PORT ${process.env.PORT}`);
+         })
+     } catch (error) {
+         console.log("error", error);
+         throw error
+     }
+ })()
+ */
